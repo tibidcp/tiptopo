1af08178aad8b6f2df25bf7af5fd90a51432fcd7
Tibeikin Oleg <tibidcp@gmail.com>
2021-03-16 14:30:12 +0300

    add project fragment

---
+++ app/src/main/java/com/tibi/tiptopo/presentation/project/ProjectFragment.kt
@@ -0,0 +1,7 @@
+package com.tibi.tiptopo.presentation.project
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ProjectFragment : Fragment()
---
+++ app/src/main/java/com/tibi/tiptopo/presentation/project/ProjectViewModel.kt
@@ -0,0 +1,8 @@
+package com.tibi.tiptopo.presentation.project
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ProjectViewModel @Inject constructor() : ViewModel()
